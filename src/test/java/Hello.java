79c1991f36cb6a97d385f4459fa8350c8bf4d99e
ybalkas <66921507+ybalkas@users.noreply.github.com>
2020-10-13 18:33:59 +0100

    update hello

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,8 +2,9 @@ public class Hello {
 
     public static void main(String[] args) {
 
-        System.out.println("Sill really FROM GITHUB MADE SOME CHANGES awesome hello");
-
+        System.out.println("Sill really some changed online");
+         System.out.println("Sill really FROM GITHUB MADE SOME CHANGES awesome hello");
+       
     }
 
 }
